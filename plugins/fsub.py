aeaccd7ca693e64012d2e5180055d92138f5a9fb
Jeol Paul <123471314+Itz-JEOL@users.noreply.github.com>
2023-05-24 16:12:00 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -1,11 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG
-
 import asyncio
 from pyrogram import Client, enums
 from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from Script import script
 
 from database.join_reqs import JoinReqs
 from info import REQ_CHANNEL, AUTH_CHANNEL, JOIN_REQS_DB, ADMINS
@@ -16,20 +13,19 @@ logger = getLogger(__name__)
 INVITE_LINK = None
 db = JoinReqs
 
-async def ForceSub(bot: Client, event: Message, file_id: str = False, mode="checksub"):
-
+async def ForceSub(bot: Client, update: Message, file_id: str = False, mode="checksub"):
     global INVITE_LINK
-    auth = ADMINS.copy() + [1125210189]
-    if event.from_user.id in auth:
+    auth = ADMINS
+    if update.from_user.id in auth:
         return True
 
     if not AUTH_CHANNEL and not REQ_CHANNEL:
         return True
 
     is_cb = False
-    if not hasattr(event, "chat"):
-        event.message.from_user = event.from_user
-        event = event.message
+    if not hasattr(update, "chat"):
+        update.message.from_user = update.from_user
+        update = update.message
         is_cb = True
 
     # Create Invite Link if not exists
@@ -37,7 +33,7 @@ async def ForceSub(bot: Client, event: Message, file_id: str = False, mode="chec
         # Makes the bot a bit faster and also eliminates many issues realted to invite links.
         if INVITE_LINK is None:
             invite_link = (await bot.create_chat_invite_link(
-                chat_id=(int(AUTH_CHANNEL) if not REQ_CHANNEL and JOIN_REQS_DB else REQ_CHANNEL),
+                chat_id=(int(AUTH_CHANNEL) if not REQ_CHANNEL and not JOIN_REQS_DB else REQ_CHANNEL),
                 creates_join_request=True if REQ_CHANNEL and JOIN_REQS_DB else False
             )).invite_link
             INVITE_LINK = invite_link
@@ -47,12 +43,12 @@ async def ForceSub(bot: Client, event: Message, file_id: str = False, mode="chec
 
     except FloodWait as e:
         await asyncio.sleep(e.x)
-        fix_ = await ForceSub(bot, event, file_id)
+        fix_ = await ForceSub(bot, update, file_id)
         return fix_
 
     except Exception as err:
         print(f"Unable to do Force Subscribe to {REQ_CHANNEL}\n\nError: {err}\n\n")
-        await event.reply(
+        await update.reply(
             text="Something went Wrong.",
             parse_mode=enums.ParseMode.MARKDOWN,
             disable_web_page_preview=True
@@ -60,15 +56,15 @@ async def ForceSub(bot: Client, event: Message, file_id: str = False, mode="chec
         return False
 
     # Mian Logic
-    if REQ_CHANNEL and JOIN_REQS_DB and db().isActive():
+    if REQ_CHANNEL and db().isActive():
         try:
             # Check if User is Requested to Join Channel
-            user = await db().get_user(event.from_user.id)
-            if user and user["user_id"] == event.from_user.id:
+            user = await db().get_user(update.from_user.id)
+            if user and user["user_id"] == update.from_user.id:
                 return True
         except Exception as e:
             logger.exception(e, exc_info=True)
-            await event.reply(
+            await update.reply(
                 text="Something went Wrong.",
                 parse_mode=enums.ParseMode.MARKDOWN,
                 disable_web_page_preview=True
@@ -76,39 +72,51 @@ async def ForceSub(bot: Client, event: Message, file_id: str = False, mode="chec
             return False
 
     try:
+        if not AUTH_CHANNEL:
+            raise UserNotParticipant
         # Check if User is Already Joined Channel
         user = await bot.get_chat_member(
-                   chat_id=(int(AUTH_CHANNEL) if not REQ_CHANNEL and JOIN_REQS_DB else REQ_CHANNEL), 
-                   user_id=event.from_user.id
+                   chat_id=(int(AUTH_CHANNEL) if not REQ_CHANNEL and not db().isActive() else REQ_CHANNEL), 
+                   user_id=update.from_user.id
                )
         if user.status == "kicked":
             await bot.send_message(
-                chat_id=event.from_user.id,
+                chat_id=update.from_user.id,
                 text="Sorry Sir, You are Banned to use me.",
                 parse_mode=enums.ParseMode.MARKDOWN,
                 disable_web_page_preview=True,
-                reply_to_message_id=event.message_id
+                reply_to_message_id=update.message_id
             )
             return False
 
         else:
             return True
     except UserNotParticipant:
-        text = "**♦️ READ THIS INSTRUCTION ♦️\n\n🗣 നിങ്ങൾ ചോദിക്കുന്ന സിനിമകൾ നിങ്ങൾക്ക് ലഭിക്കണം എന്നുണ്ടെങ്കിൽ നിങ്ങൾ ഞങ്ങളുടെ ചാനലിൽ ജോയിൻ ചെയ്തിരിക്കണം. ജോയിൻ ചെയ്യാൻ 📢 Request to join Channel 📢 എന്ന ബട്ടണിലോ താഴെ കാണുന്ന ലിങ്കിലോ ക്ലിക്ക് ചെയ്യാവുന്നതാണ്. Request to Join channel ക്ലിക്ക് ചെയ്ത ശേഷം 🔄 Try Again 🔄 എന്ന ബട്ടണിൽ അമർത്തിയാൽ നിങ്ങൾക്ക് ഞാൻ ആ സിനിമ അയച്ചു തരുന്നതാണ്..😍\n\n🗣 In Order To Get The Movie Requested By You in Our Group, You Must Have To Join Our Official Channel First By Clicking 📢 Request to Join Channel 📢 Button or the Link shown Below. After That, Click 🔄 Try Again 🔄 Button. I'll Send You That Movie 🙈\n\n👇 CLICK REQUEST TO JOIN CHANNEL & CLICK TRY AGAIN 👇**"
+        text="""**♦️ READ THIS INSTRUCTION ♦️
+        
+🗣 നിങ്ങൾ ചോദിക്കുന്ന സിനിമകൾ നിങ്ങൾക്ക് ലഭിക്കണം എന്നുണ്ടെങ്കിൽ നിങ്ങൾ ഞങ്ങളുടെ ചാനലിൽ ജോയിൻ ചെയ്തിരിക്കണം. ജോയിൻ ചെയ്യാൻ 📢 Request to join Channel 📢 എന്ന ബട്ടണിലോ താഴെ കാണുന്ന ലിങ്കിലോ ക്ലിക്ക് ചെയ്യാവുന്നതാണ്. Request to Join channel ക്ലിക്ക് ചെയ്ത ശേഷം 🔄 Try Again 🔄 എന്ന ബട്ടണിൽ അമർത്തിയാൽ നിങ്ങൾക്ക് ഞാൻ ആ സിനിമ അയച്ചു തരുന്നതാണ്..😍
+
+🗣 In Order To Get The Movie Requested By You in Our Group, You Must Have To Join Our Official Channel First By Clicking 📢 Request to Join Channel 📢 Button or the Link shown Below. After That, Click 🔄 Try Again 🔄 Button. I'll Send You That Movie 🙈
+
+👇 CLICK REQUEST TO JOIN CHANNEL & CLICK TRY AGAIN 👇**"""
+
         buttons = [
             [
-                InlineKeyboardButton("📢 Request to join Channel 📢", url=invite_link)
+                InlineKeyboardButton("📢 Request to Join Channel 📢", url=invite_link)
             ],
             [
-                InlineKeyboardButton(" 🔄 Try Again", callback_data=f"{mode}#{file_id}")
+                InlineKeyboardButton(" 🔄 Try Again 🔄 ", callback_data=f"{mode}#{file_id}")
+            ],
+            [
+               InlineKeyboardButton("🤔 Hᴇʏ Bᴏᴛ....! Wʜʏ I'ᴍ Jᴏɪɴɪɴɢ", url="https://graph.org/W%CA%9C%CA%8F-I%E1%B4%8D-J%E1%B4%8F%C9%AA%C9%B4%C9%AA%C9%B4%C9%A2-01-07")
             ]
         ]
-        
+
         if file_id is False:
             buttons.pop()
 
         if not is_cb:
-            await event.reply(
+            await update.reply(
                 text=text,
                 quote=True,
                 reply_markup=InlineKeyboardMarkup(buttons),
@@ -118,12 +126,12 @@ async def ForceSub(bot: Client, event: Message, file_id: str = False, mode="chec
 
     except FloodWait as e:
         await asyncio.sleep(e.x)
-        fix_ = await ForceSub(bot, event, file_id)
+        fix_ = await ForceSub(bot, update, file_id)
         return fix_
 
     except Exception as err:
         print(f"Something Went Wrong! Unable to do Force Subscribe.\nError: {err}")
-        await event.reply(
+        await update.reply(
             text="Something went Wrong.",
             parse_mode=enums.ParseMode.MARKDOWN,
             disable_web_page_preview=True
