53fce50e60f22a6b7e4ee4101904f0e7bec87175
Cinemahubbots <126318209+Cinemahubbots@users.noreply.github.com>
2023-06-02 22:07:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = '10001717'
 API_HASH = 'c8cdc2079f7ab083e644381740260265'
-BOT_TOKEN = '6236356662:AAHtNHhv1U2SK4yh5VGY20AoaZ9Zb_L0rrs'
+BOT_TOKEN = '6236356662:AAGrzE3Mw6lC5RDXiBOJnRhg6XFAp7rPrcU'
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -35,10 +35,10 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # FSUB
-auth_channel = environ.get('AUTH_CHANNEL', '-1001676067733')
+auth_channel = environ.get('AUTH_CHANNEL', '-1001827621958')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 # Set to False inside the bracket if you don't want to use Request Channel else set it to Channel ID
-REQ_CHANNEL = environ.get("REQ_CHANNEL", '-1001676067733')
+REQ_CHANNEL = environ.get("REQ_CHANNEL", '-1001827621958')
 REQ_CHANNEL = int(REQ_CHANNEL) if REQ_CHANNEL and id_pattern.search(REQ_CHANNEL) else False
 JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
