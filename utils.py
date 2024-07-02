3d3d9c48d0be89849eb154871731b442ae587388
Cinemahubbots <126318209+Cinemahubbots@users.noreply.github.com>
2024-07-02 07:41:01 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -73,8 +73,6 @@ async def is_subscribed(bot, query):
         else:
             return False
 
-
-
 async def get_poster(query, bulk=False, id=False, file=None):
     if not id:
         # https://t.me/GetTGLink/4183
